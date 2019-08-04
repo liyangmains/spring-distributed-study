bd7810836b0b5a6bb96898fedc2cf20497efc853
liyangmains <1301934707@qq.com>
2019-08-05 00:06:23 +0800

    修改MyApplication注释

--- spring-distributed-dubbo-provider-day8/src/main/java/com/ly/MyApplication.java
+++ spring-distributed-dubbo-provider-day8/src/main/java/com/ly/MyApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 /**
+ * 服务提供端
  * demo地址https://www.cnblogs.com/zjfjava/p/9696086.html
  * 开启基于注解的dubbo功能（主要是包扫描@DubboComponentScan）
  * 也可以在配置文件中使用dubbo.scan.base-package来替代   @EnableDubbo
